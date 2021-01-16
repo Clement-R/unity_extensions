d9e692555f5a7d83fb016e1fe84d53fda6469949
C.Rondeau <rondeau.clement@gmail.com>
2021-01-16 16:15:45 +0100

    + add Singleton class from https://github.com/tlauterbach/potato-util/blob/main/Assets/PotatoUtil/Scripts/Singleton.cs

---
+++ UnityUtils/Misc/Singleton.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Cake.Utils
+{
+    /// <summary>
+    /// Monobehaviour based singleton class that allows only
+    /// internal access to the singleton itself. Any external
+    /// access must be dictated/allowed by the inheriting class.
+    /// </summary>
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Returns whether there is no active singleton (TRUE) 
+        /// or if one is active and assigned (FALSE)
+        /// </summary>
+        public static bool IsNull
+        {
+            get
+            {
+                return m_instance == null;
+            }
+        }
+
+        protected static T Instance
+        {
+            get
+            {
+                if (IsNull)
+                {
+                    throw new Exception(string.Format(
+                        "No singleton of type `{0}' has been initialized!",
+                        typeof(T).Name
+                    ));
+                }
+                return m_instance;
+            }
+        }
+        private static T m_instance = null;
+
+        [SerializeField, Tooltip("If the singleton already exists and is destroyed, " +
+            "does it destroy only the component (FALSE) or will it also destroy " +
+            "the GameObject the component is attached to (TRUE).")]
+        private bool m_destroyGameObject = false;
+
+        private void Awake()
+        {
+            if (m_instance == null)
+            {
+                m_instance = this as T;
+                OnAwake();
+            }
+            else if (m_destroyGameObject)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+        private void OnDestroy()
+        {
+            if (m_instance == this)
+            {
+                m_instance = null;
+                OnDestroyed();
+            }
+        }
+
+        protected virtual void OnAwake()
+        {
+            // override to implement custom behaviour
+        }
+        protected virtual void OnDestroyed()
+        {
+            // override to implement custom behaviour
+        }
+    }
+}
